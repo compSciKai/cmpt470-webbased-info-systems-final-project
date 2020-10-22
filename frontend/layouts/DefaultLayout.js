095ff2d491d43cd587244309e2d19d4f6656d603
Adam <cadamrun@gmail.com>
2020-10-21 22:56:08 -0700

    The start of the dashboard

--- frontend/layouts/DefaultLayout.js
+++ frontend/layouts/DefaultLayout.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Notification } from 'grommet-icons';
-import { Box, Button, Collapsible, Heading, Text } from 'grommet';
+import { Menu } from 'grommet-icons';
+import { Box, Button, Collapsible, Heading, Text, Main } from 'grommet';
 
 export function DefaultLayout(props) {
   const [nav, setNav] = useState(false);
@@ -21,18 +21,18 @@ export function DefaultLayout(props) {
           <strong>Bubbly</strong>
         </Heading>
         <Button onClick={() => setNav(!nav)}
-          icon={<Notification color="white"/>}
+          icon={<Menu color="white"/>}
         />
       </Box>
       <Box flex direction="row">
 {/* Content */}
-        <Box flex align="center" justify="center">
+        <Box fill align="center" pad="medium">
           {props.children}
         </Box>
 {/* Nav Bar */}
         <Collapsible direction="horizontal" open={nav}>
           <Box flex width="medium" background="light-2" pad="small" elevation="small">
-            <Text size="xlarge">Nav Bar</Text>
+            <Text size="xlarge">TODO ADD ICONS</Text>
           </Box>
         </Collapsible>
       </Box>
