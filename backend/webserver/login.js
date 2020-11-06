e037759a3ff346bb946e964a270aade8150da5df
zxzhao <zxzhao@sfu.ca>
2020-11-05 19:31:57 -0800

    Refactor file location

--- backend/webserver/login.js
+++ backend/webserver/login.js
@@ -1,1 +1,11 @@
-document.console.log("Hello World");
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+var remember_me = document.getElementById("Remember_Me");
+var message = document.getElementById("message");
+
+document.getElementById("form").addEventListener("onsubmit", function (){
+
+    console.log(username.value);
+    message.innerText = username.value;
+
+});
