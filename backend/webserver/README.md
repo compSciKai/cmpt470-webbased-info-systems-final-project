895bfedee96f05c77ce62bcee1cc4a39fa4a0a9f
zxzhao <zxzhao@sfu.ca>
2020-11-05 19:54:08 -0800

    Commit before dev branch

--- backend/webserver/README.md
+++ backend/webserver/README.md
@@ -3,6 +3,6 @@
 - Users will be able to sign in with a known credential if they have previously signed up
 - Users will be able to authenticate with google (tentative)
 ## Passport 
-A middleware to help authenticate (with google or mongoDB) <br />
+A middleware to help authenticate (with serverside stored credentials, google, or mongoDB) <br />
 Run the following commands in the main directory to install dependencies <br/> <br/>
 `npm install passport` <br/> <br/>
--- backend/webserver/login.js
+++ backend/webserver/login.js
@@ -1,11 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('view-engine', 'ejs');
+
 var username = document.getElementById("username");
 var password = document.getElementById("password");
 var remember_me = document.getElementById("Remember_Me");
 var message = document.getElementById("message");
+var form = document.getElementById("form");
+form.onsubmit = function f(e){
+    e.preventDefault();
 
-document.getElementById("form").addEventListener("onsubmit", function (){
-
-    console.log(username.value);
-    message.innerText = username.value;
-
-});
+}
