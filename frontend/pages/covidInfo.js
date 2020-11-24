342d0092782699ec8acf257aaba8a7e6766e2e42
Joergen Reyes <joergenr@sfu.ca>
2020-11-23 20:22:29 -0800

    Add source to covid info page

--- frontend/pages/covidInfo.js
+++ frontend/pages/covidInfo.js
@@ -1,5 +1,6 @@
 import {Box, Text} from "grommet"
 import Head from 'next/head'
+import Link from "next/link"
 
 export default function CovidInfo(props){
   return (
@@ -7,8 +8,16 @@ export default function CovidInfo(props){
       <Head>
         <title>Bubbly | COVID-19 INFO</title> 
       </Head>
-      <Text size="xlarge" margin="small" alignSelf="center">COVID-19 Info</Text>
-      <Text size="medium" margin="small" alignSelf="center">BubbleBot retrieves COVID-19 data from several sources.</Text>
+      <Box alignSelf="center">
+        <Text size="xlarge" margin={{vertical: "small"}}>COVID-19 Info</Text>
+        <Text size="medium" margin={{vertical: "small"}}>BubbleBot retrieves COVID-19 data from several sources.</Text>
+
+        <Link margin={{vertical: "small"}} href="http://www.bccdc.ca/health-info/diseases-conditions/covid-19/data">
+          <a>BC Center For Disease Control</a>
+        </Link>
+        
+      </Box>
+      
     </Box>
   )
 }
