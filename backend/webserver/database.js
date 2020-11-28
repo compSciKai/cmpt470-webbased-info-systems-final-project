6e328ec1b5559da4338d6191b0118b438ad10d47
zxzhao <zxzhao@sfu.ca>
2020-11-28 05:14:51 -0800

    Add simple caching abilities to database library (read, write, clear)

--- backend/webserver/database.js
+++ backend/webserver/database.js
@@ -1,34 +1,56 @@
 // Holds and manages the connection to the database.
 // This is passed into other components so they can use the database without caring too much about the overhead of connecting/disconnecting.
 // May also perform queries or updates to it with functions, or just leave it to other components to do that. (Undecided)
-const mongoose = require('mongoose');
-const URI = 'mongodb://admin:bbbot123@mongo.bubble-bot.com:27017/admin';
-
-const Stuff = mongoose.model('anything', {title: {type: String, require: false}});
+const URI = 'mongodb://admin:bbbot123@mongo.bubble-bot.com:27017/bbbot?authSource=admin';
+const {MongoClient} = require('mongodb');
 
 class DatabaseServices {
 
-    constructor() {
-        //Connect to database
-        console.log(URI);
-        mongoose.connect(URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }).then((result)=> {
-            console.log('connection to db successful')
-        }).catch((err) => {
-            console.log('failed to connect to db')
-        })
-
+     constructor() {
 
     }
-
+    //Choose which model we're saving to
+    saveToTable(collection, data, callback){
+        const client = new MongoClient(URI,{ useUnifiedTopology: true });
+        client.connect()
+            .then(r => {
+                client.db('bbbot').collection(collection).insertMany(data)
+                    .then(r => {
+                        client.close().then(r => callback());
+                    })
+                    .catch((err) => console.log(err));
+            })
+            .catch((err) => console.log(err));
+    }
     //which model to pull
-    getModel(){
+    getTable(collection, search_terms, callback) {
+         let resultArray = [];
+         MongoClient.connect(URI,{ useUnifiedTopology: true }, function (err, db){
+             if (err) throw err;
+             let dbo = db.db('bbbot');
+             let cursor = dbo.collection(collection).find(search_terms);
+             cursor.forEach(function (doc, err){
+                 resultArray.push(doc);
+             }, function (){
+                 db.close();
+                 callback(resultArray);
+             })
 
+         })
+        return resultArray;
     }
-    getDb() {
-        //return this.db;
+
+    clearTable(collection, callback) {
+        MongoClient.connect(URI,{ useUnifiedTopology: true }, function (err, db){
+            if (err) throw err;
+            let dbo = db.db('bbbot');
+            dbo.collection(collection).remove(function (err, delOK){
+                if (err) throw err;
+                if (delOK) console.log("table cleared");
+                db.close();
+                callback();
+            });
+        })
     }
 }
 
--- backend/webserver/database_test.js
+++ backend/webserver/database_test.js
@@ -4,9 +4,28 @@ const app = express();
 
 const db = new DatabaseServices();
 
+//save test
+db.saveToTable('test', [{one:5, two:6}, {one:7, two:7}, {one:8, two:8}], function (){
+    //callback for after saving to table
+    console.log('doing stuff after saving to table');
+})
+
+//read test
+db.getTable('test', {one: 7}, function (result){
+    console.log(result);
+});
+
+//read test 2 (if this can happen asynchronously)
+let x = db.getTable('test'); //then do stuff with x
+
+
+//clear test (clears a table of saved content)
+db.clearTable('test', function(){
+    console.log('doing stuff after clearing the table')
+});
 
 app.get('/', (req, res) => {
-    res.send('party party');
+    res.send(x);
 });
 
 app.listen(8080);
